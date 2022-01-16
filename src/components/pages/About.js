5f7ea7572109d29af51c93b12f3c4ab02e3ac3d8
Shao <shaotang.yen@gmail.com>
2022-01-17 00:40:44 +1100

    adding text animation

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import profilePic from "../assets/Shao-photo-2.jpg";
+import TypeAnimation from 'react-type-animation';
 
 export default function About() {
   return (
@@ -12,10 +13,27 @@ export default function About() {
           This is <span class="accent-color">Shao</span>, currently based in Sydney, Australia. Been in digital marketing and event management in video game industry for 9 years. Studied <span class="accent-color">Full-Stack Web Development</span> bootcamp of Sydney University last half year and worked on many web projects which helped me developed essential skills. I also have a strong Computer Science and Math background.
           </p>
           <p class="fst-italic">
-            #HTML #CSS #JavaScript #Node #MySQL #React #Java #Photoshop #Premiere #AfterEffects #MSOffice
+            {/* #HTML #CSS #JavaScript #Node #MySQL #React #Java #Photoshop #Premiere #AfterEffects #MSOffice */}
+            {/* Found this interesting lib: https://www.npmjs.com/package/react-type-animation for typing animation */}
+            <TypeAnimation
+              cursor={false}
+              sequence={ 
+                ['#HTML #CSS #JavaScript #Node #MySQL #React #Java #Photoshop #Premiere #AfterEffects #MSOffice']
+              }
+              repeat='1'
+              />
           </p>
           <p>
-            Feel free to reach out.
+          <TypeAnimation
+              cursor={false}
+              sequence={ 
+                ['Hello, welcome!', 3000,
+                  'Natigation on the top right meun', 3000,
+                'Feel free to reach out', 10000]
+              }
+              cursor='true'
+              repeat='Infinity'
+              />
           </p>
         </div>
       </div>
